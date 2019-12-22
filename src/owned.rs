ec56d2c99a4f5040956deb8036b0e78bb7a62bd6
Stephan Hügel <urschrei@gmail.com>
2019-12-22 12:54:48 +0000

    Use a single example GeoJSON file for all examples

--- src/owned.rs
+++ src/owned.rs
@@ -49,37 +49,7 @@ fn process_geojson(gj: GeoJson) {
 }
 
 fn main() {
-    let geojson_str = r#"
-    {
-      "type": "GeometryCollection",
-      "geometries": [
-        {"type": "Point", "coordinates": [0,1]},
-        {"type": "MultiPoint", "coordinates": [[-1,0],[1,0]]},
-        {"type": "LineString", "coordinates": [[-1,-1],[1,-1]]},
-        {"type": "MultiLineString", "coordinates": [
-          [[-2,-2],[2,-2]],
-          [[-3,-3],[3,-3]]
-        ]},
-        {"type": "Polygon", "coordinates": [
-          [[-5,-5],[5,-5],[0,5],[-5,-5]],
-          [[-4,-4],[4,-4],[0,4],[-4,-4]]
-        ]},
-        { "type": "MultiPolygon", "coordinates": [[
-          [[-7,-7],[7,-7],[0,7],[-7,-7]],
-          [[-6,-6],[6,-6],[0,6],[-6,-6]]
-        ],[
-          [[-9,-9],[9,-9],[0,9],[-9,-9]],
-          [[-8,-8],[8,-8],[0,8],[-8,-8]]]
-        ]},
-        {"type": "GeometryCollection", "geometries": [
-          {"type": "Polygon", "coordinates": [
-            [[-5.5,-5.5],[5,-5],[0,5],[-5,-5]],
-            [[-4,-4],[4,-4],[0,4],[-4.5,-4.5]]
-          ]}
-        ]}
-      ]
-    }
-    "#;
+    let geojson_str = include!("test.geojson");
     let geojson = geojson_str.parse::<GeoJson>().unwrap();
     process_geojson(geojson);
 }
